4f22b8b69b9f3f4804dc26314e43acb3d3e432a7
amrrs <1littlecoder@gmail.com>
2022-01-18 01:55:22 +0530

    added. columns

--- pencilsketch_webapp.py
+++ pencilsketch_webapp.py
@@ -1,7 +1,7 @@
-import streamlit as st
-import numpy as np
-from PIL import Image
-import cv2
+import streamlit as st #web app and camera
+import numpy as np # for image processing 
+from PIL import Image #Image processing 
+import cv2 #computer vision 
 
 def dodgeV2(x, y):
     return cv2.divide(x, 255 - y, scale=256)
@@ -17,6 +17,8 @@ def pencilsketch(inp_img):
 st.title("PencilSketcher App - updated with Github Dev")
 st.write("This Web App is to help convert your photos to realistic Pencil Sketches")
 
+# collect the user input 
+
 file_image = st.sidebar.file_uploader("Upload your Photos", type=['jpeg','jpg','png'])
 
 if file_image is None:
@@ -25,10 +27,13 @@ if file_image is None:
 else:
     input_img = Image.open(file_image)
     final_sketch = pencilsketch(np.array(input_img))
-    st.write("**Input Photo**")
-    st.image(input_img, use_column_width=True)
-    st.write("**Output Pencil Sketch**")
-    st.image(final_sketch, use_column_width=True)
+    one, two = st.columns(2)
+    with one:
+        st.write("**Input Photo**")
+        st.image(input_img, use_column_width=True)
+    with two:
+        st.write("**Output Pencil Sketch**")
+        st.image(final_sketch, use_column_width=True)
     if st.button("Download Sketch Images"):
         im_pil = Image.fromarray(final_sketch)
         im_pil.save('final_image.jpeg')
